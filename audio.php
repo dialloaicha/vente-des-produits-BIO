b57e6351cc233b306a0c95296fc3a96b3d61666b
dialloaicha <114631253+dialloaicha@users.noreply.github.com>
2022-11-06 15:37:02 +0000

    Add files via upload

---
+++ audio.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title></title>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="style2.css">
+		</head>
+		<body>
+			<header>
+				<p class="im1"><img src="image/7.jpg"></p>
+				
+				</p>
+			</header>
+			
+			<nav>
+				<ul class="menuHorizontal">
+					<li><a href="video.php">VIDEOS</a> </li>
+					<li><a href="image.php">IMAGES</a> </li>
+					<li><a href="ressource.php">audio</a> </li>
+					<li><a href="divers.php">DIVERS</a> </li>
+				</ul>
+			</nav>
+			<h1> musique:<audio src="audio/d4.mp3"controls></audio>
+		
+			<nav>
+				<ul class="menu2">
+					<li><a href="pageaccueil.php">Accueil</a> </li>
+					<li><a href="apropos.php">A propos</a> </li>
+					<li><a href="actualité.php">Actualités</a> </li>
+					<li><a href="contact.php">Contact</a> </li>
+				</ul>
+			</nav>
+			<section align="center">
+				<div  class="num1">
+				<img src="ima/17283867-gâteau-d-anniversaire.jpg">
+				<img src="ima/comment-decorer-un-gateau-anniversaire.jpg">
+				<img src="ima/ima.jpg">
+				</div>
+						
+				<div class="num2">
+				<form action="traitement.php" method="post">
+                <h2 class="text-center">Inscription</h2>       
+                <div class="form-group">
+                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password_retype" class="form-control" placeholder="Re-tapez le mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block">Inscription</button>
+                </div>   
+            </form>
+				</div>
+			
+				<div class="num3"></div>
+			</section>
+		
+			<section>
+				<div class="num4"></div>
+			</section>
+			</section>
+			<footer>
+				<p>&copy copy right tous droits reservés!</p>
+			</footer>
+				</body>
+	</html>		
---
+++ utilisateurs.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : dim. 07 nov. 2021 à 20:18
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `youtube`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+CREATE TABLE `utilisateurs` (
+  `id` int(11) NOT NULL,
+  `pseudo` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` text NOT NULL,
+  `ip` varchar(20) NOT NULL,
+  `date_inscription` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `utilisateurs`
+--
+ALTER TABLE `utilisateurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
